477ba2f3a9e56a4a29ebadc9a1cdc12bd9936c6e
Open_Source_Lover <justamanuelax@gmail.com>
2025-01-17 08:13:46 +0200

    Travellist with no comments

---
+++ src/Item.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+
+export default function Item({ item, onDeleteItem, onToggleItem }) {
+    Item.propTypes = {
+           item: PropTypes.array,
+           onDeleteItem: PropTypes.func,
+           onToggleItem: PropTypes.func
+       }
+   
+    return (
+      <li>
+        <input
+          type="checkbox"
+          value={item.packed}
+          onChange={() => onToggleItem(item.id)}
+        />
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+          {item.quantity} {item.description}
+        </span>
+        <button onClick={() => onDeleteItem(item.id)}>❌</button>
+      </li>
+    );
+  }
+  
---
+++ src/Logo.jsx
@@ -0,0 +1,7 @@
+export default function Logo(){
+return(
+    <div>
+        <h1>🌴 Far Away💼</h1>
+    </div>
+)
+}
